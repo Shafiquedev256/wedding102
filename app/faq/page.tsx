3108c60baac6495b43a5a540dbe675eb5289b962
Musinguzi Shafiq <149778835+Shafiquedev256@users.noreply.github.com>
2025-12-25 14:14:02 +0300

    Update Navigation component to use scrolled state

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,10 +1,18 @@
+import {useState, useEffect} from "react" 
 import FAQSection from "../components/FAQ";
 import Navigation from "../components/navbar";
 
 const SchedulePage = () => {
+  const [scrolled, setScrolled] =useState(false) 
+  useEffect(() => {
+    const handleScroll = () => setScrolled(window.scrollY > 50);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <Navigation scrolled={true} />
+      <Navigation scrolled />
       <FAQSection />
     </>
   );

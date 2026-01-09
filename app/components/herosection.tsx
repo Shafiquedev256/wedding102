45a919dfd17deb9f94be8209648abd4e963c30f0
shafiq <shafiq@gmail.com>
2026-01-09 15:04:49 +0300

    new

--- app/components/herosection.tsx
+++ app/components/herosection.tsx
@@ -6,15 +6,21 @@ import Link from "next/link";
 import { useTranslate } from "@/app/hooks/useTranslate";
 import { useLanguage } from "@/app/context/LanguageContext";
 
+// Define the Language type (all lowercase)
+type Language = "en" | "ca";
+
 export default function Hero() {
   const [scrolled, setScrolled] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-  const { language, setLanguage } = useLanguage();
+
+  const { language, setLanguage } = useLanguage(); // no generic needed
 
   const title = useTranslate("The wedding of");
   const you = useTranslate("YOU");
   const are = useTranslate("ARE");
   const invited = useTranslate("INVITED");
+
+  const languages: Language[] = ["en", "ca"];
 
   useEffect(() => {
     const handleScroll = () => setScrolled(window.scrollY > 50);
@@ -23,8 +29,6 @@ export default function Hero() {
   }, []);
 
   const closeMenu = () => setMenuOpen(false);
-
-  const languages = ["EN", "CA"]; // example languages
 
   return (
     <div className="relative min-h-screen text-white md:h-screen bg-[url('/IMG-20251226-WA0000.jpg')] bg-center bg-cover flex flex-col">
@@ -36,39 +40,41 @@ export default function Hero() {
             : "absolute bg-transparent"
         }`}
       >
-        <div className="max-w-7xl mx-auto px-6 py-6 flex items-center justify-between relative">
-          {/* Logo - left */}
-          <Link href="/">
+        <div className='max-w-7xl mx-auto px-6 py-6 flex items-center justify-between relative'>
+          {/* Logo */}
+          <Link href='/'>
             <Image
-              src={`${scrolled ? "/logo.png" : "/black3.png"}`}
-              alt="Wedding Logo"
+              src={scrolled ? "/logo.png" : "/black3.png"}
+              alt='Wedding Logo'
               width={40}
               height={40}
-              className="object-contain"
+              className='object-contain'
             />
           </Link>
 
-          {/* Language Toggle - always centered */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 z-50">
+          {/* Language Toggle */}
+          <div className='absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 z-50'>
             <select
               value={language}
-              onChange={(e) => setLanguage(e.target.value)}
-              className={`px-2 py-1  focus:outline-none  ${
-                scrolled ?"bg-[#FDF8F5] text-black"  : "bg-transparant text-white"
+              onChange={(e) => setLanguage(e.target.value as Language)}
+              className={`px-2 py-1 focus:outline-none ${
+                scrolled
+                  ? "bg-[#FDF8F5] text-black"
+                  : "bg-transparent text-white"
               }`}
             >
               {languages.map((lang) => (
                 <option key={lang} value={lang} className='text-black'>
-                  {lang}
+                  {lang.toUpperCase()}
                 </option>
               ))}
             </select>
           </div>
 
-          {/* Desktop Menu - right */}
-          <div className="hidden md:flex gap-8 items-center">
+          {/* Desktop Menu */}
+          <div className='hidden md:flex gap-8 items-center'>
             <Link
-              href="/schedule"
+              href='/schedule'
               className={`text-sm tracking-wider hover:${
                 scrolled ? "text-[#7D2E3D]" : "text-white"
               } transition-colors`}
@@ -76,7 +82,7 @@ export default function Hero() {
               SCHEDULE
             </Link>
             <Link
-              href="/travel"
+              href='/travel'
               className={`text-sm tracking-wider hover:${
                 scrolled ? "text-[#7D2E3D]" : "text-white"
               } transition-colors`}
@@ -84,7 +90,7 @@ export default function Hero() {
               TRAVEL & STAY
             </Link>
             <Link
-              href="/faq"
+              href='/faq'
               className={`text-sm tracking-wider hover:${
                 scrolled ? "text-[#7D2E3D]" : "text-white"
               } transition-colors`}
@@ -92,7 +98,7 @@ export default function Hero() {
               FAQ
             </Link>
             <Link
-              href="/contact"
+              href='/contact'
               className={`text-sm tracking-wider hover:${
                 scrolled ? "text-[#7D2E3D]" : "text-white"
               } transition-colors`}
@@ -100,8 +106,8 @@ export default function Hero() {
               CONTACT
             </Link>
             <Link
-              href="/rsvp"
-              className="bg-[#7D2E3D] text-white px-6 py-2 text-sm tracking-wider hover:bg-[#5D1E2D] transition-colors"
+              href='/rsvp'
+              className='bg-[#7D2E3D] text-white px-6 py-2 text-sm tracking-wider hover:bg-[#5D1E2D] transition-colors'
             >
               RSVP
             </Link>
@@ -109,9 +115,9 @@ export default function Hero() {
 
           {/* Mobile Hamburger */}
           <button
-            aria-label="Open menu"
+            aria-label='Open menu'
             onClick={() => setMenuOpen(true)}
-            className="md:hidden absolute right-6 top-1/2 transform -translate-y-1/2 z-50"
+            className='md:hidden absolute right-6 top-1/2 transform -translate-y-1/2 z-50'
           >
             <i
               className={`ri-menu-3-line text-3xl ${
@@ -123,48 +129,48 @@ export default function Hero() {
 
         {/* Mobile Fullscreen Menu */}
         {menuOpen && (
-          <div className="md:hidden fixed top-0 left-0 w-full h-full py-10 z-50 bg-white">
+          <div className='md:hidden fixed top-0 left-0 w-full h-full py-10 z-50 bg-white'>
             <button
-              aria-label="Close menu"
+              aria-label='Close menu'
               onClick={closeMenu}
-              className="absolute top-6 right-6 text-[48px] leading-none font-light text-[#3E2723] hover:opacity-70 transition"
+              className='absolute top-6 right-6 text-[48px] leading-none font-light text-[#3E2723] hover:opacity-70 transition'
             >
               &times;
             </button>
 
-            <div className="flex flex-col justify-center items-center h-full space-y-16 px-6">
+            <div className='flex flex-col justify-center items-center h-full space-y-16 px-6'>
               <Link
-                href="/schedule"
+                href='/schedule'
                 onClick={closeMenu}
-                className="uppercase tracking-[2px] text-sm border-b border-gray-200 pb-2 text-[#3E2723]"
+                className='uppercase tracking-[2px] text-sm border-b border-gray-200 pb-2 text-[#3E2723]'
               >
                 SCHEDULE
               </Link>
               <Link
-                href="/travel"
+                href='/travel'
                 onClick={closeMenu}
-                className="uppercase tracking-[2px] text-sm border-b border-gray-200 pb-2 text-[#3E2723]"
+                className='uppercase tracking-[2px] text-sm border-b border-gray-200 pb-2 text-[#3E2723]'
               >
                 TRAVEL & STAY
               </Link>
               <Link
-                href="/faq"
+                href='/faq'
                 onClick={closeMenu}
-                className="uppercase tracking-[2px] text-sm border-b border-gray-200 pb-2 text-[#3E2723]"
+                className='uppercase tracking-[2px] text-sm border-b border-gray-200 pb-2 text-[#3E2723]'
               >
                 FAQ
               </Link>
               <Link
-                href="/contact"
+                href='/contact'
                 onClick={closeMenu}
-                className="uppercase tracking-[2px] text-sm border-b border-gray-200 pb-2 text-[#3E2723]"
+                className='uppercase tracking-[2px] text-sm border-b border-gray-200 pb-2 text-[#3E2723]'
               >
                 CONTACT
               </Link>
               <Link
-                href="/rsvp"
+                href='/rsvp'
                 onClick={closeMenu}
-                className="bg-[#7D2E3D] w-60 text-center text-white px-6 py-2 text-sm tracking-wider hover:bg-[#5D1E2D] transition-colors"
+                className='bg-[#7D2E3D] w-60 text-center text-white px-6 py-2 text-sm tracking-wider hover:bg-[#5D1E2D] transition-colors'
               >
                 RSVP
               </Link>
@@ -174,16 +180,16 @@ export default function Hero() {
       </nav>
 
       {/* Hero Content */}
-      <div className="flex flex-1 flex-col items-center justify-center px-6 text-center">
-        <div className="max-w-4xl w-full space-y-6 md:space-y-8">
-          <div className="p-2 mt-12"></div>
-          <div className="relative pt-6">
-            <div className="absolute top-0 left-0 w-full text-white text-center font-serif italic text-sm md:text-base tracking-widest">
+      <div className='flex flex-1 flex-col items-center justify-center px-6 text-center'>
+        <div className='max-w-4xl w-full space-y-6 md:space-y-8'>
+          <div className='p-2 mt-12'></div>
+          <div className='relative pt-6'>
+            <div className='absolute top-0 left-0 w-full text-white text-center font-serif italic text-sm md:text-base tracking-widest'>
               {title}
             </div>
             <h1
               style={{ fontFamily: "Playfair Display, serif" }}
-              className="md:mt-0 mt-6 flex justify-center items-center space-x-3 text-white text-[60px] md:text-7xl lg:text-9xl leading-tight"
+              className='md:mt-0 mt-6 flex justify-center items-center space-x-3 text-white text-[60px] md:text-7xl lg:text-9xl leading-tight'
             >
               <span>Gemma</span>
               <span>&</span>
@@ -191,23 +197,27 @@ export default function Hero() {
             </h1>
           </div>
 
-          <div className="p-2 md:pb-2 pb-4 md:mb-18 h-10 md:mt-0 md:pt-2 pt-8 md:h-20"></div>
+          <div className='p-2 md:pb-2 pb-4 md:mb-18 h-10 md:mt-0 md:pt-2 pt-8 md:h-20'></div>
 
-          <div className="flex justify-center mt-12 gap-4 md:gap-8 text-xs sm:text-sm md:text-base tracking-widest text-white">
+          <div className='flex justify-center mt-12 gap-4 md:gap-8 text-xs sm:text-sm md:text-base tracking-widest text-white'>
             <span>{you}</span>
             <span>{are}</span>
             <span>{invited}</span>
           </div>
 
-          <div className="w-full p-2 space-y-4 flex flex-col justify-center items-center">
-            <div className="flex flex-row space-x-3 text-white justify-center items-center">
-              <span className="text-center whitespace-nowrap">12 . 06 . 2026 </span>
+          <div className='w-full p-2 space-y-4 flex flex-col justify-center items-center'>
+            <div className='flex flex-row space-x-3 text-white justify-center items-center'>
+              <span className='text-center whitespace-nowrap'>
+                12 . 06 . 2026
+              </span>
               <span>|</span>
-              <span className="whitespace-nowrap">BELL RECO , ARGENTONA , SPAIN</span>
+              <span className='whitespace-nowrap'>
+                BELL RECO , ARGENTONA , SPAIN
+              </span>
             </div>
             <Link
-              href="/rsvp"
-              className="inline-block bg-[#7D2E3D] my-3  text-white px-8 py-3  text-sm tracking-wider hover:bg-[#5D1E2D] transition-colors"
+              href='/rsvp'
+              className='inline-block bg-[#7D2E3D] my-3 text-white px-8 py-3 text-sm tracking-wider hover:bg-[#5D1E2D] transition-colors'
             >
               RSVP
             </Link>
